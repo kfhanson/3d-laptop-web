6ffbf731ffb5c123cac88f142e5885fb2fd8469d
kfhanson <kfhanson.2508@gmail.com>
2024-12-26 22:48:12 +0700

    show base model

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,20 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import LaptopModel from './Laptop'
+import React from "react";
+import { Canvas } from "@react-three/fiber";
+import { OrbitControls, Environment } from "@react-three/drei";
+import LaptopModel from "./Laptop"; // Import LaptopModel
 
 function App() {
-
   return (
-    <div className="App">
-      <LaptopModel />
+    <div style={{ width: "100vw", height: "100vh" }}>
+      <Canvas>
+        <ambientLight intensity={0.5} />
+        <directionalLight position={[10, 10, 10]} />
+        <LaptopModel />
+        <Environment preset="sunset" />
+        <OrbitControls />
+      </Canvas>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/Laptop.jsx
+++ src/Laptop.jsx
@@ -1,27 +1,8 @@
-import React, { useRef, useEffect } from 'react'
-import { Canvas, useFrame, useLoader } from '@react-three/fiber'
-import { OrbitControls, PerspectiveCamera, Environment } from '@react-three/drei'
-import { GLTFLoader } from 'three/examples/jsm/Addons.js'
+import React from "react";
+import { useGLTF } from "@react-three/drei";
 
-function Model() {
-    const gltf = useLoader(GLTFLoader, './public/laptop.glb')
-    const modelRef = useRef()
-    // useFrame((state, delta) => {
-    //     if (modelRef.current) {
-    //         modelRef.current.rotation.y += delta * 0.5
-    //     }
-    // })
-    return <primitive object={gltf.scene} ref={modelRef} scale={[1, 1, 1]} />
+function LaptopModel() {
+  const { scene } = useGLTF("/laptop.glb"); // Make sure the file path matches
+  return <primitive object={scene} scale={2} />;
 }
-
-export default function LaptopModel() {
-    return (
-        <Canvas style={{ width: '100vw', height: '100vh' }}>
-            <PerspectiveCamera makeDefault position={[0, 0, 5]} />
-            <ambientLight intensity={0.5} />
-            <pointLight position={[10, 10, 10]} />
-            <Model />
-            <OrbitControls />
-        </Canvas>
-    )
-}
+export default LaptopModel;
